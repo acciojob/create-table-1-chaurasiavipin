0779ed9687bfb1aa27b5ea8cc4a4267d37180516
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-14 22:39:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
+	 const table = document.getElementById("myTable");
+      const rowCount = table.rows.length;
+      const newRow = table.insert_Row();
+
+      const cell1 = newRow.insertCell(0);
+      const cell2 = newRow.insertCell(1);
+
+      cell1.textContent = `Row${rowCount + 1} cell1`;
+      cell2.textContent = `Row${rowCount + 1} cell2`;
   
   
 }
