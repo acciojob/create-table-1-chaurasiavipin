d74c939ded608e6ade60b4ac8d7c13990257e97a
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-15 16:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 function insert_Row() {
-    //Write your code here
-	 const table = document.getElementById("sampleTable");
-let tr = document.createElement("tr");
-	let td=document.createElement("td");
-	let tdd=document.createElement("td");
-	
-	
-	
-	
-      const rowCount = table.rows.length;
-      const newRow = table.insert_Row();
-
-      const cell1 = newRow.insertCell(0);
-      const cell2 = newRow.insertCell(1);
-
-      cell1.textContent = `Row${rowCount + 1} cell1`;
-      cell2.textContent = `Row${rowCount + 1} cell2`;
-  
-  
+	let myTal=document.getElementById("sampleTable");
+	var row=myTal.insertRow(0);
+	var cell1=row.insertCell(0);
+	var cell2=row.insertCell(1);
+	cell1.textContent="New Cell1";
+	cell2.textContent="New Cell2";
 }
